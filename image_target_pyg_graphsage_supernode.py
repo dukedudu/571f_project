d56b910cc7f96831a2400a67b2bea747a5b8c791
Duke Du <dukedu98@gmail.com>
2022-12-03 20:21:35 -0800

    correct supernode code

--- image_target_pyg_graphsage_supernode.py
+++ image_target_pyg_graphsage_supernode.py
@@ -14,9 +14,14 @@ import random, pdb, math, copy
 from tqdm import tqdm
 from scipy.spatial.distance import cdist
 from sklearn.metrics import confusion_matrix
+import torch.nn.functional as F
 import networkx as nx
 from collections import defaultdict
-from node2vec import Node2Vec
+# from node2vec import Node2Vec
+from torch_geometric.nn import GraphSAGE
+
+
+# from torch_geometric.nn import Node2Vec
 
 def op_copy(optimizer):
     for param_group in optimizer.param_groups:
@@ -124,118 +129,6 @@ def cal_acc(loader, netF, netB, netC, flag=False):
     else:
         return accuracy*100, mean_ent
 
-def train_target(args):
-    dset_loaders = data_load(args)
-    ## set base network
-    if args.net[0:3] == 'res':
-        netF = network.ResBase(res_name=args.net).cuda()
-    elif args.net[0:3] == 'vgg':
-        netF = network.VGGBase(vgg_name=args.net).cuda()  
-
-    netB = network.feat_bottleneck(type=args.classifier, feature_dim=netF.in_features, bottleneck_dim=args.bottleneck).cuda()
-    netC = network.feat_classifier(type=args.layer, class_num = args.class_num, bottleneck_dim=args.bottleneck).cuda()
-
-    modelpath = args.output_dir_src + '/source_F.pt'   
-    netF.load_state_dict(torch.load(modelpath))
-    modelpath = args.output_dir_src + '/source_B.pt'   
-    netB.load_state_dict(torch.load(modelpath))
-    modelpath = args.output_dir_src + '/source_C.pt'    
-    netC.load_state_dict(torch.load(modelpath))
-    netC.eval()
-    for k, v in netC.named_parameters():
-        v.requires_grad = False
-
-    param_group = []
-    for k, v in netF.named_parameters():
-        if args.lr_decay1 > 0:
-            param_group += [{'params': v, 'lr': args.lr * args.lr_decay1}]
-        else:
-            v.requires_grad = False
-    for k, v in netB.named_parameters():
-        if args.lr_decay2 > 0:
-            param_group += [{'params': v, 'lr': args.lr * args.lr_decay2}]
-        else:
-            v.requires_grad = False
-
-    optimizer = optim.SGD(param_group)
-    optimizer = op_copy(optimizer)
-
-    max_iter = args.max_epoch * len(dset_loaders["target"])
-    interval_iter = max_iter // args.interval
-    iter_num = 0
-
-    while iter_num < max_iter:
-        try:
-            inputs_test, _, tar_idx = iter_test.next()
-        except:
-            iter_test = iter(dset_loaders["target"])
-            inputs_test, _, tar_idx = iter_test.next()
-
-        if inputs_test.size(0) == 1:
-            continue
-
-        if iter_num % interval_iter == 0 and args.cls_par > 0:
-            netF.eval()
-            netB.eval()
-            mem_label = obtain_label(dset_loaders['test'], netF, netB, netC, args)
-            mem_label = torch.from_numpy(mem_label).cuda()
-            netF.train()
-            netB.train()
-
-        inputs_test = inputs_test.cuda()
-
-        iter_num += 1
-        lr_scheduler(optimizer, iter_num=iter_num, max_iter=max_iter)
-
-        features_test = netB(netF(inputs_test))
-        outputs_test = netC(features_test)
-
-        if args.cls_par > 0:
-            pred = mem_label[tar_idx]
-            classifier_loss = nn.CrossEntropyLoss()(outputs_test, pred)
-            classifier_loss *= args.cls_par
-            if iter_num < interval_iter and args.dset == "VISDA-C":
-                classifier_loss *= 0
-        else:
-            classifier_loss = torch.tensor(0.0).cuda()
-
-        if args.ent:
-            softmax_out = nn.Softmax(dim=1)(outputs_test)
-            entropy_loss = torch.mean(loss.Entropy(softmax_out))
-            if args.gent:
-                msoftmax = softmax_out.mean(dim=0)
-                gentropy_loss = torch.sum(-msoftmax * torch.log(msoftmax + args.epsilon))
-                entropy_loss -= gentropy_loss
-            im_loss = entropy_loss * args.ent_par
-            classifier_loss += im_loss
-
-        optimizer.zero_grad()
-        classifier_loss.backward()
-        optimizer.step()
-
-        if iter_num % interval_iter == 0 or iter_num == max_iter:
-            netF.eval()
-            netB.eval()
-            if args.dset=='VISDA-C':
-                acc_s_te, acc_list = cal_acc(dset_loaders['test'], netF, netB, netC, True)
-                log_str = 'Task: {}, Iter:{}/{}; Accuracy = {:.2f}%'.format(args.name, iter_num, max_iter, acc_s_te) + '\n' + acc_list
-            else:
-                acc_s_te, _ = cal_acc(dset_loaders['test'], netF, netB, netC, False)
-                log_str = 'Task: {}, Iter:{}/{}; Accuracy = {:.2f}%'.format(args.name, iter_num, max_iter, acc_s_te)
-
-            args.out_file.write(log_str + '\n')
-            args.out_file.flush()
-            print(log_str+'\n')
-            netF.train()
-            netB.train()
-
-    if args.issave:   
-        torch.save(netF.state_dict(), osp.join(args.output_dir, "target_F_" + args.savename + ".pt"))
-        torch.save(netB.state_dict(), osp.join(args.output_dir, "target_B_" + args.savename + ".pt"))
-        torch.save(netC.state_dict(), osp.join(args.output_dir, "target_C_" + args.savename + ".pt"))
-        
-    return netF, netB, netC
-
 
 def train_target_node2vec(args):
     dset_loaders = data_load(args)
@@ -303,21 +196,23 @@ def train_target_node2vec(args):
         features_test = netB(netF(inputs_test))
         outputs_test = netC(features_test)
 
-        if args.cls_par > 0:
-            tar_idx_batch = tar_idx.detach().clone().cpu().numpy().tolist()
-            tar_idx_batch = [str(ele) for ele in tar_idx_batch]
-            # print(tar_idx_batch)
-            # print(fea_lookup)
-            # print(fea_lookup['57'])
-            feat_from_node2vec = torch.from_numpy(fea_lookup[tar_idx_batch]).float().to(torch.device("cuda:0"))
+        if args.cls_par > 0:  # batch level
+            # node[tar_idx] = features_test
+            # edge[tar_idx] = nn.Softmax(dim=1)(outputs_test)
+            # pred = mem_label[tar_idx]  # feature bank
+            # test 1: using numpy array
+            tar_idx_batch = tar_idx.detach().clone().cpu().numpy()
+            # todo: customize mse loss
+            # todo: try different losses (KL divergence)
             feat_loss = nn.MSELoss()
-            feat_dist_loss = torch.mean(feat_loss(features_test, feat_from_node2vec))
+            feat_dist_loss = torch.mean(feat_loss(features_test, fea_lookup[tar_idx_batch].to(torch.device("cuda:0"))))
             feat_dist_loss *= args.cls_par
             if iter_num < interval_iter and args.dset == "VISDA-C":
                 feat_dist_loss *= 0
         else:
-            feat_dist_loss = torch.tensor(0.0).to(torch.device("cuda:0"))
-
+            # classifier_loss = torch.tensor(0.0).cuda()
+            # classifier_loss = torch.tensor(0.0)
+            feat_dist_loss = torch.tensor(0.0).cuda()
         if args.ent:
             softmax_out = nn.Softmax(dim=1)(outputs_test)
             entropy_loss = torch.mean(loss.Entropy(softmax_out))
@@ -362,62 +257,6 @@ def print_args(args):
         s += "{}:{}\n".format(arg, content)
     return s
 
-def obtain_label(loader, netF, netB, netC, args):
-    start_test = True
-    with torch.no_grad():
-        iter_test = iter(loader)
-        for _ in range(len(loader)):
-            data = iter_test.next()
-            inputs = data[0]
-            labels = data[1]
-            inputs = inputs.cuda()
-            feas = netB(netF(inputs))
-            outputs = netC(feas)
-            if start_test:
-                all_fea = feas.float().cpu()
-                all_output = outputs.float().cpu()
-                all_label = labels.float()
-                start_test = False
-            else:
-                all_fea = torch.cat((all_fea, feas.float().cpu()), 0)
-                all_output = torch.cat((all_output, outputs.float().cpu()), 0)
-                all_label = torch.cat((all_label, labels.float()), 0)
-
-    all_output = nn.Softmax(dim=1)(all_output)
-    ent = torch.sum(-all_output * torch.log(all_output + args.epsilon), dim=1)
-    unknown_weight = 1 - ent / np.log(args.class_num)
-    _, predict = torch.max(all_output, 1)
-
-    accuracy = torch.sum(torch.squeeze(predict).float() == all_label).item() / float(all_label.size()[0])
-    if args.distance == 'cosine':
-        all_fea = torch.cat((all_fea, torch.ones(all_fea.size(0), 1)), 1)
-        all_fea = (all_fea.t() / torch.norm(all_fea, p=2, dim=1)).t()
-
-    all_fea = all_fea.float().cpu().numpy()
-    K = all_output.size(1)
-    aff = all_output.float().cpu().numpy()
-
-    for _ in range(2):
-        initc = aff.transpose().dot(all_fea)
-        initc = initc / (1e-8 + aff.sum(axis=0)[:,None])
-        cls_count = np.eye(K)[predict].sum(axis=0)
-        labelset = np.where(cls_count>args.threshold)
-        labelset = labelset[0]
-
-        dd = cdist(all_fea, initc[labelset], args.distance)
-        pred_label = dd.argmin(axis=1)
-        predict = labelset[pred_label]
-
-        aff = np.eye(K)[predict]
-
-    acc = np.sum(predict == all_label.float().numpy()) / len(all_fea)
-    log_str = 'Accuracy = {:.2f}% -> {:.2f}%'.format(accuracy * 100, acc * 100)
-
-    args.out_file.write(log_str + '\n')
-    args.out_file.flush()
-    print(log_str+'\n')
-
-    return predict.astype('int')
 
 def obtain_vec(loader, netF, netB, netC, args):
     print("generating node embedding")
@@ -436,12 +275,6 @@ def obtain_vec(loader, netF, netB, netC, args):
             inputs = inputs.cuda()
             feas = netB(netF(inputs))
             outputs = netC(feas)
-
-            # feature (node) and score (edge) bank update
-            # output_norm = F.normalize(feas)  # might remove
-            # fea_bank[indx] = output_norm.detach().clone().cpu()
-            # score_bank[indx] = outputs.detach().clone()
-
             if start_test:
                 all_fea = feas.float().cpu()
                 all_output = outputs.float().cpu()
@@ -452,42 +285,61 @@ def obtain_vec(loader, netF, netB, netC, args):
                 all_output = torch.cat((all_output, outputs.float().cpu()), 0)
                 all_label = torch.cat((all_label, labels.float()), 0)
 
-    # previous wrong normalization
-    # all_output = nn.Softmax(dim=1)(all_output)
-    # all_fea = (all_fea.t() / torch.norm(all_fea, p=2, dim=1)).t()
+    print("generating class supernode...")
+    all_output = nn.Softmax(dim=1)(all_output)
+    all_fea = (all_fea.t() / torch.norm(all_fea, p=2, dim=1)).t()
+    _, predict = torch.max(all_output, 1)
+    K = all_output.size(1)
+    aff = all_output.float().cpu().numpy()
+    for _ in range(2):
+        initc = aff.transpose().dot(all_fea)
+        initc = initc / (1e-8 + aff.sum(axis=0)[:, None])  # super node
+        cls_count = np.eye(K)[predict].sum(axis=0)
+        labelset = np.where(cls_count > args.threshold)
+        labelset = labelset[0]
 
-    # normalize p but not f
-    all_output = (nn.Softmax(dim=1)(all_output).t() / torch.norm(all_fea, p=2, dim=1)).t()
-    # all_fea = (all_fea.t() / torch.norm(all_fea, p=2, dim=1)).t()
+        dd = cdist(all_fea, initc[labelset], args.distance)
+        pred_label = dd.argmin(axis=1)
+        predict = labelset[pred_label]
 
-    edge = all_output @ all_output.T  # edge graph
-    # print(edge.size())
-    # edge_graph pass through one more softmax?
-    # self-sampling?
-    adj = edge.detach().clone().cpu().numpy()
-    # graph = nx.complete_graph(num_sample)
-    graph = nx.Graph()
-    feature_dict = defaultdict(list)
-    for i, v in enumerate(all_fea):
-        feature_dict[i] = v
-    # print(feature_dict[0])
-    graph.add_nodes_from(np.arange(num_sample))
-    # print(len(feature_dict))
-    nx.set_node_attributes(graph, feature_dict, "feat") # value and name are switched.
+        aff = np.eye(K)[predict]
+    # feat_bank[-args.class_num:] = torch.from_numpy(initc)
+    super_node_embedding = torch.from_numpy(initc)
+    print("generating class supernode complete...")
+
+    all_fea = torch.cat((all_fea, super_node_embedding.float().cpu()), 0)
+    all_fea = (all_fea.t() / torch.norm(all_fea, p=2, dim=1)).t()
+    edge = all_fea @ all_fea.T  # edge graph
+    threshold = 0.8
+
+    edge_index = []
+    node_i = []
+    node_j = []
 
     for i in range(num_sample):
         for j in range(num_sample):
-          if args.cut_edges and adj[i][j] < args.weight_thres:
-              continue
-          graph.add_edge(i, j, weight=adj[i][j])
+            if edge[i, j] >= threshold:
+                node_i.append(i)
+                node_j.append(j)
 
-    node2vec = Node2Vec(graph, dimensions=256, walk_length=10, num_walks=200,
-                        workers=8)  # Use temp_folder for big graphs
-    model = node2vec.fit(window=10, min_count=1, batch_words=4)
-    fea_lookup = model.wv
-    print("generating node embedding complete")
-    return all_fea, graph, fea_lookup
+    for i in range(num_sample):
+        j = predict[i] + num_sample
+        node_i.append(i)
+        node_j.append(j)
 
+    edge_index.append(node_i)
+    edge_index.append(node_j)
+
+    model = GraphSAGE(all_fea.size()[1], 256, 10).to(torch.device("cuda:0"))
+    fea_lookup = model(all_fea.to(torch.device("cuda:0")), torch.LongTensor(edge_index).to(torch.device("cuda:0")))
+
+    # TODO:
+    #  1. Add edge weight
+    #  2. Try cache
+    #  3. Try different value of layers
+    #  4. Refactor
+
+    return all_fea, edge_index, fea_lookup.detach()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='SHOT')
@@ -575,6 +427,6 @@ if __name__ == "__main__":
             args.gent = ''
             args.savename = 'par_' + str(args.cls_par) + '_thr' + str(args.threshold)
         args.out_file = open(osp.join(args.output_dir, 'log_' + args.savename + '.txt'), 'w')
-        args.out_file.write(print_args(args)+'\n')
+        args.out_file.write(print_args(args) + '\n')
         args.out_file.flush()
         train_target_node2vec(args)
